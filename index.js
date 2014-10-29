ebf444dccc958c962a05a2aa8b8ab1eb635e78fb
morganherlocker <morgan.herlocker@gmail.com>
2014-10-29 11:56:36 -0400

    add basic positive slope line test

---
+++ index.js
@@ -0,0 +1,9 @@
+//assumes line is left->right
+
+module.exports = function (start, end) {
+	var stepX = 1;
+	var stepY = 1;
+	var startCell = [Math.floor(start[0]), Math.floor(start[1])];
+	var endCell = [Math.floor(end[0]), Math.floor(end[1])];
+
+}
--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 var test = require('tape');
-var trace = require('/');
+var trace = require('./');
 
 test('line-trace', function(t){
-	var start = [];
-	var end = [];
+	var start = [2.2, 3.3];
+	var end = [6.7,10.1];
 
-
-})
+	var cells = trace(start, end);
+	
+	t.equal(typeof cells[0][0], 'number');
+	t.equal(cells.length, 11);
+	t.equal(cells[0][0], 2);
+	t.equal(cells[0][1], 3);
+});
