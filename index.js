3ba9e062b27ebb57b2e9d49006d6255c4426d91d
morganherlocker <morgan.herlocker@gmail.com>
2014-10-30 14:34:51 -0400

    refactor tests and add graph illustrations

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 //assumes line is left->right
 
 module.exports = function (start, end) {
-	var stepX = 1;
-	var stepY = 1;
-	var startCell = [Math.floor(start[0]), Math.floor(start[1])];
-	var endCell = [Math.floor(end[0]), Math.floor(end[1])];
+	var cells = {};
+	var x0 = Math.floor(start[0]);
+	var y0 = Math.floor(start[1]);
+	var x1 = Math.floor(end[0]);
+	var y1 = Math.floor(end[1]);
+	var xCorrection = start[0] - x0;
+	var yCorrection = start[1] - y0;
 
+	var dx = Math.abs(x1-x0);
+	var dy = Math.abs(y1-y0);
+	var sx = (x0 < x1) ? 1 : -1;
+	var sy = (y0 < y1) ? 1 : -1;
+	var err = dx-dy;
+
+	while(true){
+		//console.log('corrections: ', xCorrection+', '+yCorrection)
+		//console.log(Math.floor(x0+xCorrection), Math.floor(y0+yCorrection))
+		//console.log(Math.floor(x1+xCorrection), Math.floor(y1+yCorrection))
+		cells[Math.floor(x0+xCorrection)+'|'+Math.floor(y0+yCorrection)] = true;
+
+		if ((Math.floor(x0)==Math.floor(x1)) && (Math.floor(y0)==Math.floor(y1))) break;
+		var e2 = 2*err;
+		if (e2 > -dy){ err -= dy; x0  += sx; }
+		else if (e2 < dx){ err += dx; y0  += sy; }
+	}
+	//console.log('====')
+	return Object.keys(cells).map(function(key){return key.split('|').map(function(v){return parseInt(v)})});
 }
+
--- test.js
+++ test.js
@@ -4,11 +4,75 @@ var trace = require('./');
 test('line-trace', function(t){
 	var start = [2.2, 3.3];
 	var end = [6.7,10.1];
-
 	var cells = trace(start, end);
-	
 	t.equal(typeof cells[0][0], 'number');
-	t.equal(cells.length, 11);
+	t.equal(cells.length, 12);
 	t.equal(cells[0][0], 2);
 	t.equal(cells[0][1], 3);
+	t.end();
+});
+
+test('line-trace -- same cells, different fractions', function(t){
+	var cells1 = trace([0.001, 1.999], [3.001, 4.999]);
+	var cells2 = trace([0.999, 1.001], [3.001, 4.999]);
+	t.deepEqual(cells1, cells2);
+	t.end();
+});
+
+
+/*
+-----------------        -----------------
+|       |x      |        |#######|#######|
+|       |       |        |#######|#######|
+|       |       |        |#######|#######|
+-----------------   =>   -----------------
+|x      |       |        |#######|       |
+|       |       |        |#######|       |
+|       |       |        |#######|       |
+-----------------        -----------------
+*/
+
+test('line-trace -- edge cell top', function(t){
+	var cells = trace([0.1, 0.9], [1.1, 1.9]);
+	t.deepEqual(cells, [[0,0], [0,1], [1,1]]);
+	console.log(cells)
+	t.end();
+});
+
+/*
+-----------------        -----------------
+|       |       |        |       |#######|
+|       |       |        |       |#######|
+|       |      x|        |       |#######|
+-----------------   =>   -----------------
+|       |       |        |#######|#######|
+|       |       |        |#######|#######|
+|      x|       |        |#######|#######|
+-----------------        -----------------
+*/
+
+test('line-trace -- edge cell bottom', function(t){
+	var cells = trace([0.9, 0.1], [1.9, 1.1]);
+	t.deepEqual(cells, [[0,0], [1,0], [1,1]]);
+	console.log(cells)
+	t.end();
+});
+
+/*
+-----------------        -----------------
+|       |       |        |#######|#######|
+|       |   x   |        |#######|#######|
+|       |       |        |#######|#######|
+-----------------   =>   -----------------
+|       |       |        |#######|#######|
+|   x   |       |        |#######|#######|
+|       |       |        |#######|#######|
+-----------------        -----------------
+*/
+
+test('line-trace -- across a diagnal', function(t){
+	var cells = trace([0.5, 0.5], [1.5, 1.5]);
+	t.deepEqual(cells, [[0,0], [0,1], [1,0], [1,1]]);
+	console.log(cells)
+	t.end();
 });
